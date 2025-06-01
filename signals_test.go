4e02e45d887b5ddeb8ba4d2574b63f31c41fc66f
heathcetide <154225089+heathcetide@users.noreply.github.com>
2025-06-01 17:31:20 +0800

    feat: add signals

---
+++ signals_test.go
@@ -0,0 +1,22 @@
+package hibiscus
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSignal(t *testing.T) {
+	var val string
+	var eid uint
+	eid = Sig().Connect("mock_test", func(sender any, params ...any) {
+		val = sender.(string)
+		assert.True(t, Sig().inLoop)
+		Sig().Disconnect("mock_test", eid)
+	})
+	Sig().Emit("mock_test", "unittest")
+	assert.Equal(t, val, "unittest")
+	assert.Equal(t, 0, len(Sig().events))
+	Sig().Clear("mock_test", SigUserResetPassword, SigUserVerifyEmail)
+	assert.Equal(t, 0, len(Sig().sigHandlers))
+}
